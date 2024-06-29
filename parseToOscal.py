a32e53749b05e4a9f0fa0a6a81aed5b2ccbc27ba
buidav <105074908+buidav@users.noreply.github.com>
2024-06-28 17:12:28 -0700

    f

--- parseToOscal.py
+++ parseToOscal.py
@@ -1,5 +1,13 @@
-if __name__ == '__main__':
-    baselines = Path('./baselines').resolve()
-    b = read_baseline_docs(baselines)
+import pandas as pd
 
-    print(json.dumps(b, indent=4))
+if __name__ == '__main__':
+    excel = pd.read_excel('./NistMapping.xlsx', sheet_name=None)
+    df = excel['mapping']
+    for i, j in df.iterrows():
+        if 'MS.AAD' not in j['SCuBA Policy ID']:
+            continue
+        # print(j[''], end=" ")
+        # print(j[1], end=" ")
+        print(str(j['NIST SP 800-53 Revision 5 ']).splitlines())
+        # print(type(j))
+        # print()
